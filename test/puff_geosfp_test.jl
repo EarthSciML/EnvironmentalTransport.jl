a498167a54b785e40bff702e8fa73113c9efdf51
Christopher Tessum <ctessum@gmail.com>
2025-05-14 19:52:01 -0500

    Get tests to pass

--- test/puff_geosfp_test.jl
+++ test/puff_geosfp_test.jl
@@ -14,8 +14,8 @@ endtime = DateTime(2022, 5, 1, 0, 1)
 
 di = DomainInfo(
     starttime, endtime;
-    lonrange = deg2rad(-115):deg2rad(-68.75),
-    latrange = deg2rad(25):deg2rad(53.7),
+    lonrange = deg2rad(-115):deg2rad(1):deg2rad(-68.75),
+    latrange = deg2rad(25):deg2rad(1):deg2rad(53.7),
     levrange = 1:15,
     dtype = Float64)
 
