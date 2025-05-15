4853b1b48e9ad68cce0246df63c17b876dae4c17
Christopher Tessum <ctessum@gmail.com>
2025-05-14 21:32:02 -0500

    Add Sofiev (2012) plume rise

--- src/EnvironmentalTransport.jl
+++ src/EnvironmentalTransport.jl
@@ -5,7 +5,7 @@ using SciMLOperators
 using LinearAlgebra
 using SciMLBase: NullParameters
 using ModelingToolkit: t, D, get_unit, getdefault, ODESystem, @variables, @parameters,
-                       @constants, get_variables, substitute,
+                       @constants, get_variables, substitute, Equation,
                        build_explicit_observed_function, setp, unknowns
 using SciMLBase: terminate!
 using DynamicQuantities: @u_str
@@ -18,5 +18,6 @@ include("advection_stencils.jl")
 include("boundary_conditions.jl")
 include("advection.jl")
 include("puff.jl")
+include("plume_rise/sofiev_2012.jl")
 
 end
---
+++ src/plume_rise/sofiev_2012.jl
@@ -0,0 +1,42 @@
+export Sofiev2012PlumeRise
+
+struct Sofiev2012PlumeRiseCoupler
+    sys
+end
+
+"""
+Wildfire plume rise model based on Sofiev et al. (2012) [1].
+
+[1] Sofiev, M., Ermakova, T., and Vankevich, R.: Evaluation of the smoke-injection height
+from wild-land fires using remote-sensing data, Atmos. Chem. Phys., 12, 1995–2006,
+https://doi.org/10.5194/acp-12-1995-2012, 2012.
+"""
+function Sofiev2012PlumeRise(; name = :Sofiev2012PlumeRise)
+    @constants begin
+        P_f0 = 1e6, [unit = u"W", description = "Reference fire power"]
+        N_0 = sqrt(2.5e-4),
+        [unit = u"1/s", description = "Reference Brunt-Vaisala frequency"]
+        α = 0.24, [description = "Empirical constant"]
+        β = 170.0, [unit = u"m", description = "Empirical constant"]
+        γ = 0.35, [description = "Empirical constant"]
+        δ = 0.6, [description = "Empirical constant"]
+    end
+
+    params = @parameters begin
+        H_abl = 1000.0, [unit = u"m", description = "Atmospheric boundary layer height"]
+        N_ft = 0.02, # TODO(CT): Calculate this from data.
+        [unit = u"1/s", description = "Free troposphere Brunt-Vaisala frequency"]
+        P_fr = 5e6, [unit = u"W", description = "Fire radiative power"]
+
+        H_p = α * H_abl + β * (P_fr / P_f0)^γ * exp(-δ * N_ft^2 / N_0^2),
+        [unit = u"m", description = "Final plume top height"]
+    end
+    ODESystem(Equation[], t, [], params; name = name,
+        metadata = Dict(:coupletype => Sofiev2012PlumeRiseCoupler))
+end
+
+function EarthSciMLBase.couple2(s::Sofiev2012PlumeRiseCoupler, p::PuffCoupler)
+    s, p = s.sys, p.sys
+    ConnectorSystem([
+        ], s, p)
+end
