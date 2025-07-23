038ffad639771452155881f24f07d78f1355443a
Christopher Tessum <ctessum@gmail.com>
2025-07-23 15:39:56 -0500

    Add ConstantBC for numerical operators

--- src/boundary_conditions.jl
+++ src/boundary_conditions.jl
@@ -1,4 +1,4 @@
-export ZeroGradBC
+export ZeroGradBC, ConstantBC
 
 """
 An array with external indexing implemented for boundary conditions.
@@ -37,3 +37,39 @@ Zero gradient boundary conditions.
 """
 struct ZeroGradBC end
 (bc::ZeroGradBC)(x) = ZeroGradBCArray(x)
+
+"""
+$(SIGNATURES)
+
+An array with zero constant boundary conditions.
+"""
+struct ConstantBCArray{P, T, N} <: BCArray{T, N}
+    parent::P
+    value::T
+    function ConstantBCArray(x::AbstractArray{T, N}, value::T) where {T, N}
+        return new{typeof(x), T, N}(x, value)
+    end
+end
+Base.size(A::ConstantBCArray) = size(A.parent)
+Base.checkbounds(::Type{Bool}, ::ConstantBCArray, i...) = true
+
+function Base.getindex(A::ConstantBCArray{P, T, N},
+        ind::Vararg{Union{Int, UnitRange}, N}) where {P, T, N}
+    v = A.parent
+    if checkbounds(Bool, v, ind...)
+        @inbounds ret = v[ind...]
+        return ret
+    end
+    return A.value
+end
+
+"""
+$(SIGNATURES)
+
+Constant boundary conditions.
+"""
+struct ConstantBC
+    value::AbstractFloat
+    ConstantBC(value::AbstractFloat) = new(value)
+end
+(bc::ConstantBC)(x) = ConstantBCArray(x, bc.value)
--- test/boundary_conditions_test.jl
+++ test/boundary_conditions_test.jl
@@ -1,13 +1,29 @@
-using Main.EnvironmentalTransport
+using EnvironmentalTransport
 using Test
 
-a = rand(3, 4)
-x = ZeroGradBC()(a)
+@testset "ZeroGradBC" begin
+    a = rand(3, 4)
+    x = ZeroGradBC()(a)
 
-@test x[1:3, 1:4] == a
-@test all(x[-10:1, 15:30] .== a[begin, end])
-@test all(x[end:(end + 20), (begin - 3):begin] .== a[end, begin])
+    @test x[1:3, 1:4] == a
+    @test all(x[-10:1, 15:30] .== a[begin, end])
+    @test all(x[end:(end + 20), (begin - 3):begin] .== a[end, begin])
 
-@test all((@view x[1:3, 1:4]) .== a)
+    @test all((@view x[1:3, 1:4]) .== a)
 
-@test CartesianIndices((3, 4)) == eachindex(x)
+    @test CartesianIndices((3, 4)) == eachindex(x)
+end
+
+@testset "ConstantBC" begin
+    v = 16.0
+    a = rand(3, 4)
+    x = ConstantBC(v)(a)
+
+    @test x[1:3, 1:4] == a
+    @test all(x[-10:1, 15:30] .== v)
+    @test all(x[end:(end + 20), (begin - 3):begin] .== v)
+
+    @test all((@view x[1:3, 1:4]) .== a)
+
+    @test CartesianIndices((3, 4)) == eachindex(x)
+end
